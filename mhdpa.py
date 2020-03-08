0e2d5e8b2fdc48871ba45e9f25d5838056d2f875
michaelrgb <michaelrgb@users.noreply.github.com>
2020-03-08 05:50:33 +0000

    Mask decoder attention only to prior entities.

--- mhdpa.py
+++ mhdpa.py
@@ -1,31 +1,5 @@
-from tfutils import *
-
-# Concat x,y coordinate onto each entity feature vector
-def concat_coord_xy(x):
-    s = x.shape.as_list()
-    f = s[1]-1.
-    coord_x = tf.range(s[1], dtype='float32')/f
-    coord_x = tf.expand_dims(tf.expand_dims([coord_x], 1), -1)
-    coord_x = tf.tile(coord_x, [1, x.shape[2].value, 1, 1])
-    f = s[2]-1.
-    coord_y = tf.range(s[2], dtype='float32')/f
-    coord_y = tf.expand_dims(tf.expand_dims([coord_y], 2), -1)
-    coord_y = tf.tile(coord_y, [1, 1, x.shape[1].value, 1])
-
-    coord = tf.concat([coord_y, coord_x], -1)
-    coord = tf.tile(coord, [s[0], 1, 1, 1])
-    return tf.concat([x, coord], -1)
-
-# Filter top K entities to reduce the N^2 search in self-attention
-def top_k_conv(x, top_k):
-    sum_features = tf.reduce_sum(x, -1)
-    top_k_idx = tf.nn.top_k(reshape_dims(sum_features, 2), top_k)[1] # 1D indices
-    #x = concat_coord_xy(x)
-    x = tf.reshape(x, [x.shape[0], x.shape[1]*x.shape[2], -1]) # 2D -> 1D
-
-    # gather_nd slices into first N dimensions, where N = indices.shape[-1]
-    x = tf.gather_nd(x, tf.tile(tf.expand_dims(top_k_idx,-1), [1,1,2]))
-    return x, top_k_idx
+import tensorflow as tf
+from tensorflow.python.layers import base
 
 def layer_norm(n):
     return tf.keras.layers.LayerNormalization(center=False, scale=False)(n)
@@ -33,7 +7,7 @@ def layer_norm(n):
 # https://arxiv.org/abs/1706.03762
 class MHDPA(base.Layer):
     # heads*d_k should be >= d_model
-    def __init__(self, heads=16, d_k=32, d_v=32, d_ff=128, d_out=None):
+    def __init__(self, heads=16, d_k=16, d_v=32, d_ff=128, d_out=None):
         super(MHDPA, self).__init__()
         self.heads = heads
         self.d_k = d_k
@@ -51,21 +25,28 @@ class MHDPA(base.Layer):
             self.key = self.add_weight('key',     [self.d_model, self.heads, self.d_k])
             self.value = self.add_weight('value', [self.d_model, self.heads, self.d_v])
 
+            # Sublayer 2
+            dim_out = self.d_out or self.d_model
+            self.kernel1 = self.add_weight('kernel1', [self.heads*self.d_v, self.d_ff or dim_out])
+            if self.d_ff:
+                self.kernel2 = self.add_weight('kernel2', [self.d_ff, dim_out])
+
         # Project each entity into q,k,v vectors
         value = tf.tensordot(x_to, self.value,   [-1, 0])
         key   = tf.tensordot(x_to, self.key,     [-1, 0])
-        key = layer_norm(key)
+        # key = layer_norm(key)
         return key, value
 
-    def apply_from(self, x_from, key, value, residual=True, expand_from=1, softmax_dim=1):
+    def apply_from(self, x_from, key, value, batch_dims=1, dp_mask=None):
         # Project each entity into q,k,v vectors
         query = tf.tensordot(x_from, self.query, [-1, 0])
-        query = layer_norm(query)
+        # query = layer_norm(query)
 
+        expand_from = 1
         expand_to = len(x_from.shape) + expand_from - (len(key.shape)-1)
         # Compare each q with every other entity k via dot-product
         for d in range(expand_from):
-            query = tf.expand_dims(query, softmax_dim)
+            query = tf.expand_dims(query, batch_dims)
         for d in range(expand_to):
             key = tf.expand_dims(key, 1)
             value = tf.expand_dims(value, 1)
@@ -73,32 +54,39 @@ class MHDPA(base.Layer):
         dot_product = tf.reduce_sum(query * key, -1)
         dot_product /= self.d_k**0.5
 
+        if dp_mask is not None:
+            if dp_mask is True: # Mask decoder attention only to prior entities
+                lower_triangular = tf.cumsum(tf.eye(tf.shape(key)[batch_dims]), 0)
+                shift = key.shape[batch_dims] - x_from.shape[batch_dims-1]
+                if shift: # Allow preprending entities that are always attended to
+                    lower_triangular = lower_triangular[shift:]
+                dp_mask = tf.tile(tf.expand_dims([lower_triangular], -1), [tf.shape(dot_product)[0], 1, 1, self.heads])
+            else: # Mask encoder with tensor of batch lengths
+                maxlen = tf.shape(x_from)[batch_dims-1]
+                length_mask = tf.expand_dims(1. - tf.cumsum(tf.one_hot(dp_mask, maxlen), batch_dims-1), batch_dims-1)
+                tile = [1]*len(length_mask.shape) + [self.heads]; tile[batch_dims-1] = maxlen
+                dp_mask = tf.tile(tf.expand_dims(length_mask, -1), tile)
+            dot_product = tf.where(tf.equal(dp_mask, 0.), tf.fill(tf.shape(dot_product), -float('inf')), dot_product)
+
         # Softmax on flattened expand_from
         if expand_from==2:
             dot_product = tf.reshape(dot_product, [s[0], s[1]*s[2], s[1],s[2], self.heads])
-        attention = tf.nn.softmax(dot_product, softmax_dim)
+        attention = tf.nn.softmax(dot_product, batch_dims)
         if expand_from==2:
             attention = tf.reshape(attention, [s[0], s[1],s[2], s[1],s[2], self.heads])
 
         # Weighted sum of attention values
         A = tf.expand_dims(attention, -1) * value
         for d in range(expand_from):
-            A = tf.reduce_sum(A, softmax_dim)
-
-        if not self.d_ff:
-            return A, attention, dot_product
-
-        ret = tf.reshape(A, [tf.shape(A)[0], -1])
-        if not self.kernel1:
-            # Sublayer 2
-            self.kernel1 = self.add_weight('kernel1', [self.heads*self.d_v, self.d_ff])
-            self.kernel2 = self.add_weight('kernel2', [self.d_ff*2, self.d_out or self.d_model])
+            A = tf.reduce_sum(A, batch_dims)
 
         # 2-layer MLP
-        mlp = double_relu(tf.matmul(ret, self.kernel1))
-        ret = tf.matmul(mlp, self.kernel2)
+        A = tf.reshape(A, tf.concat([tf.shape(A)[:batch_dims], [-1]], 0))
+        ret = tf.matmul(A, self.kernel1)
+        if self.d_ff:
+            ret = tf.matmul(tf.nn.relu(ret), self.kernel2)
 
-        if residual and ret.shape[-1] == x_from.shape[-1]: # Residual only applies to default output size
+        if ret.shape[-1] == x_from.shape[-1]: # Residual only applies to default output size
             ret += x_from
         return ret, attention, dot_product
 
--- tfutils.py
+++
@@ -1,5 +0,0 @@
-import tensorflow.compat.v1 as tf
-from tensorflow.python.layers import base
-
-concat_neg = lambda x: tf.concat([x, -x], -1)
-double_relu = lambda x: tf.nn.relu(concat_neg(x))
